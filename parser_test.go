08c22c7dafe93f3cf0377eb956301f960c09143a
eriktate <erik.tate.r@gmail.com>
2017-03-19 03:23:49 -0400

    Adding to parser test

--- parser_test.go
+++ parser_test.go
@@ -13,14 +13,20 @@ func Test_BasicParse(t *testing.T) {
 	ex1 := "10 = 10"
 	ex2 := "(12 > 8)"
 	ex3 := "(12 > 5) && (\"hello\" != \"world\")"
+	forFun := "\"does this work\" ~= \"/.*(work).*/\""
 
 	parser := decide.GetParser()
 	lexer := decide.GetLexer()
 
+	// TESTING FOR SUCCESS
+	t.Log("TESTING PARSE FOR SUCCESS")
 	result1, err := parser.Parse(lexer.Lex(ex1))
 	result2, err := parser.Parse(lexer.Lex(ex2))
 	result3, err := parser.Parse(lexer.Lex(ex3))
+	funResult, err := parser.Parse(lexer.Lex(forFun))
 
+	// ASSERT SUCCESS
+	t.Log("ASSERTING SUCCESS")
 	if err != nil {
 		t.Error(err)
 	}
@@ -34,6 +40,10 @@ func Test_BasicParse(t *testing.T) {
 	}
 
 	if !decide.Decide(result3) {
+		t.Error("Truthy expression resulted in false")
+	}
+
+	if !decide.Decide(funResult) {
 		t.Error("Truthy expression resulted in false")
 	}
 }
